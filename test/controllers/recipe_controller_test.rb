fa6dc397a23a0ed030e62913c596313b76de5f73
tryc <terashima.yuko@gmail.com>
2016-10-01 06:33:20 +0000

    Remove comment line

--- test/controllers/recipe_controller_test.rb
+++ test/controllers/recipe_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class RecipeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "should get pickup via rakuten api" do
     get :pickup
     assert_response :success
