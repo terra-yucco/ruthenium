ad8ba4caace5f3048d71ec64c4dbb554a638532d
tryc <terashima.yuko@gmail.com>
2016-10-01 01:20:11 +0000

    レシピAPI(大カテゴリ取得)疎通確認

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,12 +3,12 @@ require 'rakuten_web_service'
 class RecipeController < ApplicationController
 
   def index
-#    @title = 'abc'
     RakutenWebService.configuration do |c|
       c.application_id = ENV["APPID"]
       c.affiliate_id = ENV["AFID"]
     end
 
+    @large_categories = RakutenWebService::Recipe.large_categories
     @title = 'rakuten_recipe_test'
   end
 end
