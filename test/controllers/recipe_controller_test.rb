535570605ec2841921d3e527dcb0ad058da928c6
kiyoshi <cloudliner.jp@gmail.com>
2016-10-01 13:27:53 +0900

    fix test code

--- test/controllers/recipe_controller_test.rb
+++ test/controllers/recipe_controller_test.rb
@@ -8,6 +8,6 @@ class RecipeControllerTest < ActionController::TestCase
   test "should get pickup via rakuten api" do
     get :pickup
     assert_response :success
-    assert_not_nil assigns(@menu)
+    assert_not_nil assigns(:menu)
   end
 end
