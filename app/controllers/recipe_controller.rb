06efc2eae83f2f1ae366a1fd540601dd2852bb7f
tryc <terashima.yuko@gmail.com>
2016-10-01 00:59:54 +0000

    Add rakuten web service: wip

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,14 @@
+require 'rakuten_web_service'
+
 class RecipeController < ApplicationController
+
+  def index
+#    @title = 'abc'
+    RakutenWebService.configuration do |c|
+      c.application_id = ENV["APPID"]
+      c.affiliate_id = ENV["AFID"]
+    end
+
+    @title = 'rakuten_recipe_test'
+  end
 end
