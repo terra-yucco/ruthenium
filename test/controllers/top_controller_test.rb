6b80d2b984dc90da6e2c23328cce440110939856
b1621ry <b1621ry@aiit.ac.jp>
2016-10-01 01:47:59 +0000

    mergeしてからプッシュ

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TopControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
