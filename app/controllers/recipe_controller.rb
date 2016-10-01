8963e3adee2e8857f49aa911869fab378e91ddd3
kiyoshi <cloudliner.jp@gmail.com>
2016-10-01 11:51:30 +0900

    Add API call for pickup

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,16 +1,27 @@
 require 'rakuten_web_service'
 
 class RecipeController < ApplicationController
+  def pickup
+    rakuten_api
+    @menus = RakutenWebService::Recipe.ranking(15)
+  end
 
+  # Test Page
   def index
-    RakutenWebService.configure do |c|
-      c.application_id = ENV["APPID"]
-      c.affiliate_id = ENV["AFID"]
-    end
-
+    rakuten_api
+    
     @large_categories = RakutenWebService::Recipe.large_categories
     @menus = RakutenWebService::Recipe.ranking(15)
 
     @title = 'rakuten_recipe_test'
   end
+
+  private
+    # For Rakuten API Setting
+    def rakuten_api
+      RakutenWebService.configure do |c|
+        c.application_id = ENV["APPID"]
+        c.affiliate_id = ENV["AFID"]
+      end
+    end
 end
