ba9f64d843954b0abc374052c86490e7899dd025
kiyoshi <cloudliner.jp@gmail.com>
2016-10-01 10:13:14 +0900

    Add Route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'top#index'
 
+  # Pickup Recipe Page
+  pickup_recipe 'recipe#pickup'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
