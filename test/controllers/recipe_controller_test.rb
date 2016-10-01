eafaf8e7ad5b4f890b9780ab3a1f367db0c6c609
tryc <terashima.yuko@gmail.com>
2016-10-01 06:36:11 +0000

    Add shopping_list testcode

--- test/controllers/recipe_controller_test.rb
+++ test/controllers/recipe_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class RecipeControllerTest < ActionController::TestCase
+
   test "should get pickup via rakuten api" do
     get :pickup
     assert_response :success
     assert_not_nil assigns(:menu)
   end
+
+  test "should get shopping_list" do
+    get :shopping_list
+    assert_response :success
+    assert_not_nil assigns(:menu)
+  end
+
 end
