15d1664969071d3cfd04e3dd31be3b9ef8a34307
tryc <terashima.yuko@gmail.com>
2016-10-04 12:09:44 +0000

    Add scrape for private method too

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -56,4 +56,25 @@ class RecipeController < ApplicationController
         c.affiliate_id = ENV["AFID"]
       end
     end
+
+   # Sample for scrape
+    def scrape_by_url
+      url = 'http://recipe.rakuten.co.jp/recipe/1490006283/'
+
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset # 文字種別を取得
+        f.read # htmlを読み込んで変数htmlに渡す
+      end
+
+      # htmlをパース(解析)してオブジェクトを生成
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+
+      results = []
+      doc.css("div.materialBox").css("li").each do |result|
+        results.push(['materialName' => result.css("a").text, "materialAmount" => result.css("p").text])
+      end
+      @materials = results
+    end
+
 end
