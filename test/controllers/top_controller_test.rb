6d8952fe52c91c454876ef909eaa50d6a7bde98a
cloudliner <cloudliner.jp@gmail.com>
2016-10-28 23:02:13 +0900

    add top page test

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -5,5 +5,6 @@ class TopControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:vegetable_stocks)
   end
 end
