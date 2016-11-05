c4c6647a4984a76df745a4ff9dadf02ad3287051
b1621ry <b1621ry@aiit.ac.jp>
2016-11-05 17:06:25 +0900

    レシピ登録用scaffoldの作成

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,74 @@
+class DishesController < ApplicationController
+  before_action :set_dish, only: [:show, :edit, :update, :destroy]
+
+  # GET /dishes
+  # GET /dishes.json
+  def index
+    @dishes = Dish.all
+  end
+
+  # GET /dishes/1
+  # GET /dishes/1.json
+  def show
+  end
+
+  # GET /dishes/new
+  def new
+    @dish = Dish.new
+  end
+
+  # GET /dishes/1/edit
+  def edit
+  end
+
+  # POST /dishes
+  # POST /dishes.json
+  def create
+    @dish = Dish.new(dish_params)
+
+    respond_to do |format|
+      if @dish.save
+        format.html { redirect_to @dish, notice: 'Dish was successfully created.' }
+        format.json { render :show, status: :created, location: @dish }
+      else
+        format.html { render :new }
+        format.json { render json: @dish.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /dishes/1
+  # PATCH/PUT /dishes/1.json
+  def update
+    respond_to do |format|
+      if @dish.update(dish_params)
+        format.html { redirect_to @dish, notice: 'Dish was successfully updated.' }
+        format.json { render :show, status: :ok, location: @dish }
+      else
+        format.html { render :edit }
+        format.json { render json: @dish.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /dishes/1
+  # DELETE /dishes/1.json
+  def destroy
+    @dish.destroy
+    respond_to do |format|
+      format.html { redirect_to dishes_url, notice: 'Dish was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_dish
+      @dish = Dish.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def dish_params
+      params.require(:dish).permit(:recipe_id, :recipe_title, :img_url, :serving, :cabbage, :carrot, :onion, :pepper, :potato)
+    end
+end
---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161105080113) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.integer  "recipe_id",                null: false
+    t.string   "recipe_title",             null: false
+    t.string   "img_url",                  null: false
+    t.integer  "serving",                  null: false
+    t.integer  "cabbage",      default: 0
+    t.integer  "carrot",       default: 0
+    t.integer  "onion",        default: 0
+    t.integer  "pepper",       default: 0
+    t.integer  "potato",       default: 0
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+end
