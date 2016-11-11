4b09d10a03ce24e26840bba5b84c7d4412e0d38b
b1621ry <b1621ry@aiit.ac.jp>
2016-11-12 00:01:49 +0900

    手持ちの野菜からレシピ検索

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,20 +4,32 @@ class TopController < ApplicationController
     if cookies.permanent['veg_saved'] then
         @veg_saved = true
         @vegetable_stocks = get_vegetable_stocks cookies
+        @dishes = Array.new
 
         #手持ちの野菜から０以外を除外
         veg_list = Array.new
-        @dishes = Array.new
         for i in 0..4 do
             if @vegetable_stocks[i][2] != '0' then
+                # logger.debug @vegetable_stocks[i][0]
                 veg_list.push(@vegetable_stocks[i][0])
             end
         end
 
         #手持ちの野菜と同じものをレシピから取得
         veg_num = veg_list.length
-        for i in 0..veg_num do
-            @dishes.push(Dish.where("#{veg_list[i]} > 0"))
+        veg_ids = Array.new
+
+        for i in 0..veg_num - 1 do
+          dish_result = Dish.where("#{veg_list[i]} > 0")
+          #logger.debug veg_list[i]
+          #logger.debug dish_result.inspect
+          dish_result.each do |dish|
+            if veg_ids.include?(dish.id) then
+            else
+                veg_ids.push(dish.id)
+                @dishes.push(dish)
+            end
+          end
         end
     else
         @veg_saved = false
@@ -26,4 +38,5 @@ class TopController < ApplicationController
         render  action: :start
     end
   end
+
 end
