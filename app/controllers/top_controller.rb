b96bbcc3e47c83bcacad8bfde4103a61ef71a633
b1621ry <b1621ry@aiit.ac.jp>
2016-11-11 21:55:30 +0900

    手持ちの野菜を取得

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,15 +2,25 @@ class TopController < ApplicationController
 
   def index
     if cookies.permanent['veg_saved'] then
-      @veg_saved = true
-      @vegetable_stocks = get_vegetable_stocks cookies
-      # 5件取得
-      @dishes = Dish.take(5)
+        @veg_saved = true
+        @vegetable_stocks = get_vegetable_stocks cookies
+
+        #手持ちの野菜から０以外を除外
+        veg_list = Array.new
+        @dishes = Array.new
+        for i in 0..4 do
+            if @vegetable_stocks[i][2] != '0' then
+                veg_list.push(@vegetable_stocks[i][0])
+            end
+        end
+
+        #5件取得
+        @dishes = Dish.take(5)
     else
-      @veg_saved = false
-      @no_header = true
-      @no_footer = true
-      render  action: :start
+        @veg_saved = false
+        @no_header = true
+        @no_footer = true
+        render  action: :start
     end
   end
 end
